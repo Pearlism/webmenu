5b3016fd6cc8f61a88e08c4edb5f6b1e00a74177
Landen2rich <167775790+Pearlism@users.noreply.github.com>
2025-07-18 13:39:29 -0400

    updated menu an server

--- server.js
+++ server.js
@@ -10,18 +10,54 @@ app.use(express.static('public'));
 
 app.get('/config/:uid', (req, res) => {
   const cfgPath = path.join(CONFIG_DIR, req.params.uid + '.json');
+
   if (!fs.existsSync(cfgPath)) {
+    
     return res.json({
-      visuals: { box: true, platform: true, username: true, killcount: true, weapon: true, skeleton: true },
-      aimbot: { enable: true, show_fov: true, fov: 160, smoothness: 9, visible_check: true, current_key: 2, fovarrows: true },
-      triggerbot: { triggerbot: false, triggerbot_enable: false, delay: 1.3, distance: 50.0, key: 2 },
-      exploits: { wallbang: true },
-      colors: { namecolor: [255,255,255,255], skelcol: [255,255,255,255] }
+      visuals: {
+        box: true,
+        platform: true,
+        username: true,
+        killcount: true,
+        weapon: true,
+        skeleton: true
+      },
+      aimbot: {
+        enable: true,
+        show_fov: true,
+        fov: 160,
+        smoothness: 9,
+        vischecktext: true,
+        current_key: 2,    
+        fovarrows: true
+      },
+      triggerbot: {
+        triggerbot: false,
+        triggerbot_enable: false,
+        delay: 1.3,
+        distance: 50.0,
+        key: 2
+      },
+      exploits: {
+        wallbang: true
+      },
+      radar: {
+        radar: true,
+        posx: 35.0,
+        posy: 35.0
+      },
+      colors: {
+        namecolor: [255, 255, 255, 255],
+        skelcol: [255, 255, 255, 255],
+        rardarcolorvisible: [0, 255, 0, 255],
+        rardarcolorinvisible: [255, 0, 0, 255]
+      }
     });
   }
+
   res.sendFile(cfgPath);
 });
-// i love my shitty code haha
+
 app.post('/config/:uid', (req, res) => {
   const cfgPath = path.join(CONFIG_DIR, req.params.uid + '.json');
   fs.writeFileSync(cfgPath, JSON.stringify(req.body, null, 2));
